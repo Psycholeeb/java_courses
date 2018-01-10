09bf7608e9dedf1fca3ed7e89c901c56b41778e6
vlad.stupak <stupak_vlad@mail.ru>
2018-01-11 00:43:21 +0200

    Copy repo from root repo.

--- module8/src/main/java/com/courses/spalah/Cell.java
+++ module8/src/main/java/com/courses/spalah/Cell.java
@@ -3,5 +3,25 @@ package com.courses.spalah;
 /**
  * Класс который представляет ячейку на рулетке
  */
+
 public class Cell {
+    private int numberOfCell;
+    private int colorOfCell;    //0-green, 1-black, 2-red
+
+    public void setNumberOfCell(int numberOfCell) {
+        this.numberOfCell = numberOfCell;
+    }
+
+    public int getNumberOfCell() {
+        return numberOfCell;
+    }
+
+    public void setColorOfCell(int color) {
+        this.colorOfCell = color;
+    }
+
+    public int getColorOfCell() {
+        return colorOfCell;
+    }
 }
+
